4cf523d80bc4542ec4eb6e096fbdd87038f22304
Md Faruk Hossain <mfarukhossain63@gmail.com>
2023-05-08 14:58:44 -0500

    first commit

---
+++ PK_School/Controllers/EmployeesController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PK_School.Models;
+using System.Linq;
+
+namespace PK_School.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly EmployeeDbContext _employeeDbContext;
+        public EmployeesController(EmployeeDbContext employeeDbContext)
+        {
+            _employeeDbContext = employeeDbContext;
+        }
+        public IActionResult Index()
+        {
+            var result = _employeeDbContext.Employees.ToList();
+            return View(result);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                var emp = new Employee()
+                {
+                    Name = employee.Name,
+                    City=employee.City,
+                    State=employee.State,
+                    Salary=employee.Salary
+                };
+                _employeeDbContext.Employees.Add(emp);
+                _employeeDbContext.SaveChanges();
+                TempData["error"] = "Record Save";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Empty field Can't Submit";
+                return RedirectToAction("Index");
+            }
+           
+        }
+        
+        public IActionResult Edit(int id)
+        {
+
+                var emp = _employeeDbContext.Employees.SingleOrDefault(e => e.Id == id);            
+                var result = new Employee()
+                {
+                    Name = emp.Name,
+                    City = emp.City,
+                    State = emp.State,
+                    Salary = emp.Salary
+                };
+            return View(result);
+                     
+
+        }
+        [HttpPost]
+       public IActionResult Edit(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                var emp = new Employee()
+                {
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    City = employee.City,
+                    State = employee.State,
+                    Salary = employee.Salary
+
+                };
+                _employeeDbContext.Employees.Update(emp);
+                _employeeDbContext.SaveChanges();
+                TempData["error"] = "Record Update";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Empty field Can't Submit";
+                return RedirectToAction("Index");
+            }
+         
+        }
+        public IActionResult Delete(int id)
+        {
+            var emp = _employeeDbContext.Employees.SingleOrDefault(e=>e.Id ==id);
+            _employeeDbContext.Employees.Remove(emp);
+            _employeeDbContext.SaveChanges();
+            TempData["error"] = "Record Delete";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PK_School/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PK_School.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
